ebfee52439f3a23197490e4a6f88a1962de176ac
thiago <thiagofreitas1106@gmail.com>
2024-07-26 13:29:35 -0300

    change styles on card

--- src/components/productCard/index.jsx
+++ src/components/productCard/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Card, CardContent, CardMedia, Typography, CardActions, Button, Drawer, Box, Dialog } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, CardActions, Button, Box, Dialog, IconButton, Stack } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import Carousel from 'react-material-ui-carousel';
 
 const ProductCard = ({ images, title, description, price }) => {
@@ -10,13 +11,23 @@ const ProductCard = ({ images, title, description, price }) => {
         setDrawerOpen(open);
     };
 
-    const maxDescriptionLength = 200;
+    const maxDescriptionLength = 250;
     const isLongDescription = description.length > maxDescriptionLength;
 
     return (
         <>
-            <Card sx={{ maxWidth: 345, margin: '20px' }}>
-                <Box sx={{ height: 'auto', overflow: 'hidden', display: 'flex', alignItems: 'center' }} /*height: 240*/>
+            <Card sx={{ maxWidth: 345, margin: '10px', borderRadius: '10px', boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.25)' }}>
+                <Box sx={{ height: 'auto', overflow: 'hidden', alignItems: 'center', padding: '20px 20px 0 20px' }}>
+                    <Typography gutterBottom component="div" sx={{
+                        color: '#da5c5d',
+                        fontWeight: 'bold',
+                        fontSize: 22,
+                        lineHeight: '25px',
+                        letterSpacing: '-0.015rem',
+                        textAlign: 'center',
+                    }}>
+                        {title}
+                    </Typography>
                     {images.length > 1 ? (
                         <Carousel sx={{ width: '100%', height: '100%' }}
                             indicators={true}
@@ -25,43 +36,56 @@ const ProductCard = ({ images, title, description, price }) => {
                                 <CardMedia
                                     key={index}
                                     component="img"
-                                    height="240"
+                                    height="200"
                                     image={image}
                                     alt={`${title} image ${index + 1}`}
-                                    sx={{ objectFit: 'cover' }}
+                                    sx={{ objectFit: 'cover', borderRadius: '10px', marginTop: '10px', marginBottom: '15px', width: '100%' }}
                                 />
                             ))}
                         </Carousel>
                     ) : (
-                        <CardMedia
-                            component="img"
-                            height="240"
-                            image={images[0]}
-                            alt={title}
-                            sx={{ objectFit: 'cover', marginBottom: 4 }} //retirar o marginBottom caso remover o height auto no box principal deste componente 
-                        />
+                        <>
+                            <CardMedia
+                                component="img"
+                                height="200"
+                                image={images[0]}
+                                alt={title}
+                                sx={{ objectFit: 'cover', borderRadius: '10px', marginTop: '15px', marginBottom: '15px', width: '100%' }} //retirar o marginBottom caso remover o height auto no box principal deste componente 
+                            />
+                            <Box height={10} />
+                        </>
                     )}
                 </Box>
                 <CardContent sx={{ height: 220, overflow: 'hidden' }}>
-                    <Typography variant="h6" component="div">
-                        {price}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {isLongDescription ? (
-                            <>
-                                {showMore ? description : `${description.substring(0, maxDescriptionLength)}...`}
-                                <Button size="small" onClick={toggleDrawer(true)}>
-                                    {showMore ? 'Ver menos' : 'Ver mais'}
-                                </Button>
-                            </>
-                        ) : (
-                            description
-                        )}
-                    </Typography>
+                    <Stack gap={1}>
+                        <Stack>
+                            <Typography variant="h5" fontWeight="bold" component="div" color='#da5c5d' lineHeight='33px'>
+                                {price}
+                            </Typography>
+                        </Stack>
+                        <Stack borderTop='1px solid #E6E6E6' paddingTop={1}>
+                            <Typography variant="body" fontWeight="bold" color="#7e7e7e">
+                                Descrição
+                            </Typography>
+                        </Stack>
+                        <Stack>
 
+                            <Typography variant="body2" color="#9E9E9E" marginTop={1}>
+                                {isLongDescription ? (
+                                    <>
+                                        {showMore ? description : `${description.substring(0, maxDescriptionLength)}...`}
+                                        <Button size="small" onClick={toggleDrawer(true)}>
+                                            {showMore ? 'Ver menos' : 'Ver mais'}
+                                        </Button>
+                                    </>
+                                ) : (
+                                    <Typography variant="body2" color="#9E9E9E" marginTop={1}>
+                                        {description}
+                                    </Typography>
+                                )}
+                            </Typography>
+                        </Stack>
+                    </Stack>
                 </CardContent>
                 <CardActions>
                     <Box display="flex" width="100%">
@@ -72,10 +96,22 @@ const ProductCard = ({ images, title, description, price }) => {
 
                 </CardActions>
             </Card>
+
             <Dialog anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
                 <div style={{ width: 350, padding: 20 }}>
+                    <IconButton
+                        onClick={toggleDrawer(false)}
+                        sx={{
+                            position: 'absolute',
+                            top: 10,
+                            right: 10,
+                            color: '#da5c5d'
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
                     <Card>
-                        <Box sx={{ height: 240, overflow: 'hidden', display: 'flex', alignItems: 'center' }}>
+                        <Box sx={{ height: 'auto', overflow: 'hidden', alignItems: 'center', padding: 2 }}>
                             {images.length > 1 ? (
                                 <Carousel sx={{ width: '100%', height: '100%' }}
                                     indicators={true}
@@ -87,7 +123,7 @@ const ProductCard = ({ images, title, description, price }) => {
                                             height="240"
                                             image={image}
                                             alt={`${title} image ${index + 1}`}
-                                            sx={{ objectFit: 'cover' }}
+                                            sx={{ objectFit: 'cover', borderRadius: '10px', marginTop: '15px', marginBottom: '15px', width: '100%' }}
                                         />
                                     ))}
                                 </Carousel>
@@ -97,18 +133,25 @@ const ProductCard = ({ images, title, description, price }) => {
                                     height="240"
                                     image={images[0]}
                                     alt={title}
-                                    sx={{ objectFit: 'cover' }}
+                                    sx={{ objectFit: 'cover', borderRadius: '10px', marginTop: '15px', marginBottom: '15px', width: '100%' }}
                                 />
                             )}
                         </Box>
                         <CardContent>
-                            <Typography variant="h6" component="div">
-                                {price}
-                            </Typography>
-                            <Typography gutterBottom variant="h5" component="div">
+                            <Typography gutterBottom variant="h5" component="div" sx={{
+                                color: '#da5c5d',
+                                fontWeight: 'bold',
+                                fontSize: 22,
+                                lineHeight: '25px',
+                                letterSpacing: '-0.015rem',
+                                textAlign: 'center',
+                            }}>
                                 {title}
                             </Typography>
-                            <Typography variant="body2" color="text.secondary">
+                            <Typography variant="h5" fontWeight="bold" component="div" padding="10px 0 10px 0" lineHeight='33px' borderTop='2px solid #f1eaea' >
+                                {price}
+                            </Typography>
+                            <Typography variant="body2" color="#9E9E9E">
                                 {description}
                             </Typography>
                         </CardContent>
