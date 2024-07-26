93b8252d4c3a88fe80acad39336b5a3a9aa78ec9
thiago <thiagofreitas1106@gmail.com>
2024-07-26 12:10:29 -0300

    added carrousel

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,22 +1,20 @@
-import { Box } from '@mui/material'
+import { Box, Container, CssBaseline, Grid } from '@mui/material'
 import ProductCard from '../../productCard'
 import MenuBar from '../../menuBar'
 
 const Home = () => {
-    return (
-        <Box>
-            <MenuBar />
-            <Box display="flex" flexWrap="wrap" justifyContent="center">
-                <ProductCard
-                    image="/products/frangopassarinho.jpeg"
-                    title="Frango a passarinho"
-                    description="uma porção de frango a passarinho de 600g"
-                    price="R$19,90"
-                />
-                <ProductCard
-                    image="/products/picanha-com-arroz.jpeg"
-                    title="Picanha à moda do chef-500G"
-                    description="Picanha à Moda do Chef - 
+
+    const products = [
+        {
+            images: ["/products/frangopassarinho.jpeg"],
+            title: "Frango a passarinho",
+            description: "uma porção de frango a passarinho de 600g",
+            price: "R$19,90"
+        },
+        {
+            images: ["/products/picanha-com-arroz.jpeg", "/products/calabresa-acebolada.jpeg"],
+            title: "Picanha à moda do chef-500G",
+            description: `Picanha à Moda do Chef - 
                     500GR Uma Celebração da Carne de 
                     Qualidade Serve até 2 Pessoas 
                     Prepare-se para uma experiência 
@@ -25,61 +23,37 @@ const Home = () => {
                     suprema da qualidade e do sabor da carne, preparado de 
                     forma única pelo nosso chef mestre. Nossa picanha é cuidadosamente 
                     selecionada, garantindo a maciez e o sabor incomparáveis que fazem 
-                    dela um corte de carne tão apreciado."
-                    price="R$120,00"
-                />
-                <ProductCard
-                    image="/products/frangopassarinho.jpeg"
-                    title="Frango a passarinho"
-                    description="uma porção de frango a passarinho de 600g"
-                    price="R$19,90"
-                />
-                <ProductCard
-                    image="/products/frangopassarinho.jpeg"
-                    title="Frango a passarinho"
-                    description="uma porção de frango a passarinho de 600g"
-                    price="R$19,90"
-                />
+                    dela um corte de carne tão apreciado.`,
+            price: "R$120,00"
+        },
+        {
+            images: ["/products/frangopassarinho.jpeg"],
+            title: "Frango a passarinho",
+            description: "uma porção de frango a passarinho de 600g",
+            price: "R$19,90"
+        },
+    ]
 
-                <ProductCard
-                    image="/products/frangopassarinho.jpeg"
-                    title="Frango a passarinho"
-                    description="uma porção de frango a passarinho de 600g"
-                    price="R$19,90"
-                />
-                <ProductCard
-                    image="/products/frangopassarinho.jpeg"
-                    title="Frango a passarinho"
-                    description="uma porção de frango a passarinho de 600g"
-                    price="R$19,90"
-                />
-                <ProductCard
-                    image="/products/frangopassarinho.jpeg"
-                    title="Frango a passarinho"
-                    description="uma porção de frango a passarinho de 600g"
-                    price="R$19,90"
-                />
-                <ProductCard
-                    image="/products/frangopassarinho.jpeg"
-                    title="Frango a passarinho"
-                    description="uma porção de frango a passarinho de 600g"
-                    price="R$19,90"
-                />
-                <ProductCard
-                    image="/products/frangopassarinho.jpeg"
-                    title="Frango a passarinho"
-                    description="uma porção de frango a passarinho de 600g"
-                    price="R$19,90"
-                />
+    return (
+        <>
+            <MenuBar />
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <Grid container spacing={2}>
+                    {products.map((product, index) => (
+                        <Grid item key={index} xs={12} sm={6} md={4}>
+                            <ProductCard
+                                images={product.images}
+                                title={product.title}
+                                description={product.description}
+                                price={product.price}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </>
 
-                <ProductCard
-                    image="/products/frangopassarinho.jpeg"
-                    title="Frango a passarinho"
-                    description="uma porção de frango a passarinho de 600g"
-                    price="R$19,90"
-                />
-            </Box>
-        </Box>
     )
 }
 
--- src/components/productCard/index.jsx
+++ src/components/productCard/index.jsx
@@ -1,32 +1,128 @@
-import React from 'react';
-import { Card, CardContent, CardMedia, Typography, CardActions, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Card, CardContent, CardMedia, Typography, CardActions, Button, Drawer, Box, Dialog } from '@mui/material';
+import Carousel from 'react-material-ui-carousel';
 
-const ProductCard = ({ image, title, description, price }) => {
+const ProductCard = ({ images, title, description, price }) => {
+    const [showMore, setShowMore] = useState(false);
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleDrawer = (open) => () => {
+        setDrawerOpen(open);
+    };
+
+    const maxDescriptionLength = 200;
+    const isLongDescription = description.length > maxDescriptionLength;
+
     return (
-        <Card sx={{ maxWidth: 345, margin: '10px' }}>
-            <CardMedia
-                component="img"
-                height="300"
-                image={image}
-                alt={title}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {description}
-                </Typography>
-                <Typography variant="h6" component="div">
-                    {price}
-                </Typography>
-            </CardContent>
-            <CardActions sx={{ display: "flex", justifyContent: "center" }}>
-                <Button fullWidth variant="contained" style={{ backgroundColor: '#da5c5d' }}>
-                    Adicionar ao carrinho
-                </Button>
-            </CardActions>
-        </Card>
+        <>
+            <Card sx={{ maxWidth: 345, margin: '20px' }}>
+                <Box sx={{ height: 'auto', overflow: 'hidden', display: 'flex', alignItems: 'center' }} /*height: 240*/>
+                    {images.length > 1 ? (
+                        <Carousel sx={{ width: '100%', height: '100%' }}
+                            indicators={true}
+                        >
+                            {images.map((image, index) => (
+                                <CardMedia
+                                    key={index}
+                                    component="img"
+                                    height="240"
+                                    image={image}
+                                    alt={`${title} image ${index + 1}`}
+                                    sx={{ objectFit: 'cover' }}
+                                />
+                            ))}
+                        </Carousel>
+                    ) : (
+                        <CardMedia
+                            component="img"
+                            height="240"
+                            image={images[0]}
+                            alt={title}
+                            sx={{ objectFit: 'cover', marginBottom: 4 }} //retirar o marginBottom caso remover o height auto no box principal deste componente 
+                        />
+                    )}
+                </Box>
+                <CardContent sx={{ height: 220, overflow: 'hidden' }}>
+                    <Typography variant="h6" component="div">
+                        {price}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {isLongDescription ? (
+                            <>
+                                {showMore ? description : `${description.substring(0, maxDescriptionLength)}...`}
+                                <Button size="small" onClick={toggleDrawer(true)}>
+                                    {showMore ? 'Ver menos' : 'Ver mais'}
+                                </Button>
+                            </>
+                        ) : (
+                            description
+                        )}
+                    </Typography>
+
+                </CardContent>
+                <CardActions>
+                    <Box display="flex" width="100%">
+                        <Button variant="contained" fullWidth sx={{ backgroundColor: "#da5c5d" }}>
+                            Adicionar no carrinho
+                        </Button>
+                    </Box>
+
+                </CardActions>
+            </Card>
+            <Dialog anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+                <div style={{ width: 350, padding: 20 }}>
+                    <Card>
+                        <Box sx={{ height: 240, overflow: 'hidden', display: 'flex', alignItems: 'center' }}>
+                            {images.length > 1 ? (
+                                <Carousel sx={{ width: '100%', height: '100%' }}
+                                    indicators={true}
+                                    navButtonsAlwaysVisible={true} >
+                                    {images.map((image, index) => (
+                                        <CardMedia
+                                            key={index}
+                                            component="img"
+                                            height="240"
+                                            image={image}
+                                            alt={`${title} image ${index + 1}`}
+                                            sx={{ objectFit: 'cover' }}
+                                        />
+                                    ))}
+                                </Carousel>
+                            ) : (
+                                <CardMedia
+                                    component="img"
+                                    height="240"
+                                    image={images[0]}
+                                    alt={title}
+                                    sx={{ objectFit: 'cover' }}
+                                />
+                            )}
+                        </Box>
+                        <CardContent>
+                            <Typography variant="h6" component="div">
+                                {price}
+                            </Typography>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {title}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Box display="flex" width="100%">
+                                <Button variant="contained" fullWidth sx={{ backgroundColor: "#da5c5d" }}>
+                                    Adicionar no carrinho
+                                </Button>
+                            </Box>
+                        </CardActions>
+                    </Card>
+                </div>
+            </Dialog>
+        </>
     );
 };
 
