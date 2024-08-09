8ef8b068ee912d16c9e88be8564949fc05803b25
thiago <thiagofreitas1106@gmail.com>
2024-08-09 14:34:08 -0300

    fix some colors on searchbar and menu mobile

--- src/components/pages/catalog/searchBar/index.jsx
+++ src/components/pages/catalog/searchBar/index.jsx
@@ -18,26 +18,45 @@ const SearchBar = ({ value, onChange, placeholder }) => {
             </InputAdornment>
           ),
         }}
+
         sx={{
-          '& .MuiOutlinedInput-root': {
-            color: 'white',
-            '& fieldset': {
-              borderColor: '#ffffff',
-            },
-            '&:hover fieldset': {
-              borderColor: '#ffffff',
-            },
-            '&.Mui-focused fieldset': {
-              borderColor: '#ffffff',
-            },
-          },
           '& .MuiInputAdornment-root': {
             color: '#ffffff',
           },
           '& .MuiInputBase-input': {
             color: '#ffffff',
           },
+          '& .MuiInput-underline:before': {
+            borderBottomColor: '#ffffff', // Cor da linha antes do foco
+          },
+          '& .MuiInput-underline:hover:before': {
+            borderBottomColor: '#ffffff', // Cor da linha ao passar o mouse
+          },
+          '& .MuiInput-underline:after': {
+            borderBottomColor: '#e0e41f', // Cor da linha ao focar
+          },
         }}
+
+        // sx={{
+        //   '& .MuiOutlinedInput-root': {
+        //     color: 'white',
+        //     '& fieldset': {
+        //       borderColor: '#ffffff',
+        //     },
+        //     '&:hover fieldset': {
+        //       borderColor: '#ffffff',
+        //     },
+        //     '&.Mui-focused fieldset': {
+        //       borderColor: '#ffffff',
+        //     },
+        //   },
+        //   '& .MuiInputAdornment-root': {
+        //     color: '#ffffff',
+        //   },
+        //   '& .MuiInputBase-input': {
+        //     color: '#ffffff',
+        //   },
+        // }}
       />
     </Box>
   );
--- src/components/pagination/index.jsx
+++ src/components/pagination/index.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 import { Box, Button, Typography } from '@mui/material';
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  return (
-    <Box display="flex" justifyContent="center" alignItems="center" mt={4} color="#fff">
-      <Button
-        variant="contained"
-        disabled={currentPage === 1}
-        onClick={() => onPageChange(currentPage - 1)}
-      >
-        Anterior
-      </Button>
-      <Typography mx={2}>{`Página ${currentPage} de ${totalPages}`}</Typography>
-      <Button
-        variant="contained"
-        disabled={currentPage === totalPages}
-        onClick={() => onPageChange(currentPage + 1)}
-      >
-        Próximo
-      </Button>
-    </Box>
-  );
+    return (
+        <Box display="flex" justifyContent="center" alignItems="center" mt={4} color="#fff">
+            <Button
+                variant="contained"
+                disabled={currentPage === 1}
+                onClick={() => onPageChange(currentPage - 1)}
+            >
+                Anterior
+            </Button>
+            <Typography mx={2}>{`Página ${currentPage} de ${totalPages}`}</Typography>
+            <Button
+                variant="contained"
+                disabled={currentPage === totalPages}
+                onClick={() => onPageChange(currentPage + 1)}
+            >
+                Próximo
+            </Button>
+        </Box>
+    );
 };
 
 export default Pagination;
